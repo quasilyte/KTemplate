eede8a5060fdc047ea0ebc062c8a7cbcdcce060c
Iskander Sharipov <quasilyte@gmail.com>
2022-11-12 01:53:14 +0400

    update the benchmarks

--- benchmarks/BenchmarkRenderer.php
+++ benchmarks/BenchmarkRenderer.php
@@ -17,6 +17,11 @@ class BenchmarkRenderer {
     private $length_filter_template;
     private $default_filter_template;
     private $slot0_template;
+    private $concat2_template;
+    private $concat3_template;
+    private $concat4_template;
+    private $concat5_template;
+    private $mixed_template;
 
     /** @var Env */
     private $env;
@@ -32,7 +37,24 @@ class BenchmarkRenderer {
         $this->env = $env;
 
         $this->renderer = new Renderer($env);
-        $this->array_data_provider = new ArrayDataProvider();
+        $this->array_data_provider = new \KTemplate\ArrayDataProvider([
+            'leaf' => 10,
+            'nested' => [
+                'leaf' => 20,
+            ],
+            'x' => 123,
+            'y' => 'foo',
+            'items' => [
+                'a',
+                'b',
+                'c',
+                '',
+                'd',
+                'e',
+                'f',
+                '',
+            ],
+        ]);
         $this->null_data_provider = new NullDataProvider();
 
         $c = new Compiler();
@@ -53,6 +75,36 @@ class BenchmarkRenderer {
         $this->length_filter_template = $c->compile($env, 'test', '{% let $s = "" %}{{ $s|length }}{{ $s|length }}{{ $s|length }}{{ $s|length }}');
         $this->default_filter_template = $c->compile($env, 'test', '{% let $x = null %}{{ $x|default(0) }}{{ $x|default("a") }}{{ $x|default(1) }}{{ $x|default(null)|default(1) }}');
         $this->slot0_template = $c->compile($env, 'test', '{{null}}{{null}}{{null}}{{null}}');
+
+        $this->concat2_template = $c->compile($env, 'test', '{% let $s = "a" %}{{ $s ~ $s }}');
+        $this->concat3_template = $c->compile($env, 'test', '{% let $s = "a" %}{{ $s ~ $s ~ $s }}');
+        $this->concat4_template = $c->compile($env, 'test', '{% let $s = "a" %}{{ $s ~ $s ~ $s ~ $s }}');
+        $this->concat5_template = $c->compile($env, 'test', '{% let $s = "a" %}{{ $s ~ $s ~ $s ~ $s ~ $s }}');
+
+        $this->mixed_template = $c->compile($env, 'test', '
+            {% let $v = y %}
+            {% for $item in items %}
+                {% if $item %}
+                    > {{ $item ~ x ~ $v }}
+                {% end %}
+            {% end %}
+        ');
+    }
+
+    public function benchmarkConcat2() {
+        return $this->renderer->render($this->concat2_template, $this->array_data_provider);
+    }
+
+    public function benchmarkConcat3() {
+        return $this->renderer->render($this->concat3_template, $this->array_data_provider);
+    }
+
+    public function benchmarkConcat4() {
+        return $this->renderer->render($this->concat4_template, $this->array_data_provider);
+    }
+
+    public function benchmarkConcat5() {
+        return $this->renderer->render($this->concat5_template, $this->array_data_provider);
     }
 
     public function benchmarkSlot0() {
@@ -82,41 +134,14 @@ class BenchmarkRenderer {
     public function benchmarkVarAccess1x100() {
         return $this->renderer->render($this->var_access1_x100_template, $this->array_data_provider);
     }
+
+    public function benchmarkMixed() {
+        return $this->renderer->render($this->mixed_template, $this->array_data_provider);
+    }
 }
 
 class NullDataProvider implements DataProviderInterface {
     public function getData($key) {
-        return null;
-    }
-}
-
-class ArrayDataProvider implements DataProviderInterface {
-    private $arr_data;
-
-    public function __construct() {
-        $this->arr_data = [
-            'leaf' => 10,
-            'nested' => [
-                'leaf' => 20,
-            ],
-        ];
-    }
-
-    public function getData($key) {
-        switch ($key->part1) {
-        case 'test_name':
-            return $key->num_parts === 1 ? 'benchmark' : null;
-        case 'arr':
-            switch ($key->num_parts) {
-            case 1:
-                return $this->arr_data;
-            case 2:
-                return $this->arr_data[$key->part2];
-            default:
-                return $this->arr_data[$key->part2][$key->part3];
-            }
-        }
-
         return null;
     }
 }
